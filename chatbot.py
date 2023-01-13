2bce87a426304823c48fc19832c5064a3ff08965
Yume <104755849+Yumenin@users.noreply.github.com>
2023-01-14 02:12:41 +0800

    updated chatbot.py progress, updated data.json responses

--- chatbot.py
+++ chatbot.py
@@ -3,27 +3,38 @@ import json
 import random
 
 def ChatBot():
-    # 0: suggestions
-    # 1: user commands
+    
+    # critical file
     data = json.load(open("./data.json"))
     
     print("BOT: Hello there! I am a bot designed to help you schedule appointments with your doctor quickly and easily.\n")
-    print(f"Suggestions: \nSetting Appointment: {random.choice(data['suggestions']['SetAppointment'])}.\nCheck existing appointment: {random.choice(data['suggestions']['GetAppointment'])}\n")
+    print(f"Suggestions: \nSetting Appointment: {random.choice(data['Choices']['SetAppointment'])}.\nCheck existing appointment: {random.choice(data['Suggestions']['GetAppointment'])}\n")
     
     while(True):
         userInput = input("You: ")
-        if userInput.lower() in (response.lower() for response in data['suggestions']['SetAppointment']):
-            print(f"BOT: {random.choice(data['SetAppointment_Responses']['firstResponse'])}")
+        if userInput.lower() in (response.lower() for response in data['Suggestions']['SetAppointment']):
+            print(f"\nBOT: {random.choice(data['SetAppointment_Responses']['firstResponse'])}")
             print("Please type your full name: \n")
             userName = input("You: ")
-            print(f"BOT: {random.choice(data['SetAppointment_Responses']['secondResponse'])}")
-            print("")
+
+            print(f"\nBOT: {random.choice(data['SetAppointment_Responses']['secondResponse'])}")
+            [print(choice, end=", ") for choice in data['Medical_Departments']]
+            userChosenDepartment = input("You: ")
+            while(True):
+                if userChosenDepartment.lower() in (choice.lower() for choice in data['Medical_Departments']):
+                    break
+                print(f"BOT: {random.choice(data['Error_Responses_Department'])}")
+                userChosenDepartment = input("You: ")
+
+            print(f"BOT: {random.choice(data['SetAppointment_Responses']['thirdResponse'])}")
+            userContactNumber = input("You: ")
+            
             break
-        elif userInput.lower() in (response.lower() for response in data['suggestions']['SetAppointment']):
+        elif userInput.lower() in (response.lower() for response in data['Suggestions']['SetAppointment']):
             print("check existing appointment")
             break
         else:
-            print("Sorry, I do not recognize that command!")
+            print("\nSorry, I do not recognize that command!")
         
         
 
