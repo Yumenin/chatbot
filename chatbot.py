a0fc53cc6482e291e536de691b713bcd77e0fd8c
Yume <104755849+Yumenin@users.noreply.github.com>
2023-01-14 00:50:56 +0800

    updated chatbot.py for setting appointment

--- chatbot.py
+++ chatbot.py
@@ -7,12 +7,24 @@ def ChatBot():
     # 1: user commands
     data = json.load(open("./data.json"))
     
-    print("Hello there! I am a bot designed to help you schedule appointments with your doctor quickly and easily.")
-    print(f"Suggestions: \nSetting Appointment: {random.choice(data['suggestions']['SetAppointment'])}.\nWant to know existing appointment: {random.choice(data['suggestions']['GetAppointment'])}")
+    print("BOT: Hello there! I am a bot designed to help you schedule appointments with your doctor quickly and easily.\n")
+    print(f"Suggestions: \nSetting Appointment: {random.choice(data['suggestions']['SetAppointment'])}.\nCheck existing appointment: {random.choice(data['suggestions']['GetAppointment'])}\n")
     
     while(True):
         userInput = input("You: ")
+        if userInput.lower() in (response.lower() for response in data['suggestions']['SetAppointment']):
+            print(f"BOT: {random.choice(data['SetAppointment_Responses']['firstResponse'])}")
+            print("Please type your full name: \n")
+            userName = input("You: ")
+            print(f"BOT: {random.choice(data['SetAppointment_Responses']['secondResponse'])}")
+            print("")
+            break
+        elif userInput.lower() in (response.lower() for response in data['suggestions']['SetAppointment']):
+            print("check existing appointment")
+            break
+        else:
+            print("Sorry, I do not recognize that command!")
         
-        pass
+        
 
 ChatBot()
