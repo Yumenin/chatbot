c7e6a6e753f156df43b1011f25c3af0b69ea765e
Yume <104755849+Yumenin@users.noreply.github.com>
2023-01-17 01:20:07 +0800

    updated latest files

--- chatbot.py
+++ chatbot.py
@@ -1,60 +1,84 @@
 
 import json
 import random
-import os.path as path
+import os
+from datetime import datetime
 from collections import deque
 import secrets
 import string
 
+
 def ChatBot():
-    
     # critical file
-    data = json.load(open("./data.json"))
+    file = open("./data.json")
+    data = json.load(file)
     #dataPatients = json.load(open("./patients.json", "a")) if path.exists("patients.json") else json.load(open("./patients.json", "w"))
 
-    print("BOT: Hello there! I am a bot designed to help you schedule appointments with your doctor quickly and easily.\n")
-    print(f"Suggestions: \nSetting Appointment: {random.choice(data['Choices']['SetAppointment'])}.\nCheck existing appointment: {random.choice(data['Suggestions']['GetAppointment'])}\n")
+    print("BOT: Hello there! I am a bot designed to help you schedule appointments with your doctor quickly and easily\n")
+    print(f"Suggestions: \nSetting Appointment: {random.choice(data['Choices']['SetAppointment'])}.\nCheck existing appointment: {random.choice(data['Choices']['GetAppointment'])}\n")
     
     while(True):
         userInput = input("You: ")
-        if userInput.lower() in (response.lower() for response in data['Suggestions']['SetAppointment']):
+        if userInput.lower() in (response.lower() for response in data['Choices']['SetAppointment']):
             print(f"\nBOT: {random.choice(data['SetAppointment_Responses']['firstResponse'])}")
             print("Please type your full name: \n")
             userFullName = input("You: ")
 
             print(f"\nBOT: {random.choice(data['SetAppointment_Responses']['secondResponse'])}")
             [print(choice, end=", ") for choice in data['Medical_Departments']]
-            userChosenDepartment = input("You: ")
+            userChosenDepartment = input("\n\nYou: ")
             while(True):
                 if userChosenDepartment.lower() in (choice.lower() for choice in data['Medical_Departments']):
                     break
-                print(f"BOT: {random.choice(data['Error_Responses_Department'])}")
-                userChosenDepartment = input("You: ")
+                print(f"\nBOT: {random.choice(data['Error_Responses_Department'])}")
+                userChosenDepartment = input("\nYou: ")
 
-            print(f"BOT: {random.choice(data['SetAppointment_Responses']['thirdResponse'])}")
-            userContactNumber = input("You: ")
+            print(f"\nBOT: {random.choice(data['SetAppointment_Responses']['thirdResponse'])}")
+            userContactNumber = input("\nYou: ")
             
+            patient = {}
             randomCode = ''.join(secrets.choice(string.ascii_letters + string.digits) for repetitions in range(random.randint(4,10)))
-            if path.exists('patients.json'):
-                queue = deque([])
-                queue.append({"patientName": userFullName, "patientCheckupDepartment": userChosenDepartment, "userContactNumber": userContactNumber, "userAppointmentCode": randomCode})
-                newPatientsData = open("patients.json", "w")
-                newPatientsData.write(json.dumps(list(queue)))
-                newPatientsData.close()
+
+            if os.path.isfile("./patients.json") and os.stat("./patients.json").st_size != 0:
+                existingFile = open("./patients.json", "r+")
+                rawData = json.load(existingFile)
+                queue = deque(rawData['patients'])
+                newPatient = {randomCode: {"patientName": userFullName, "patientCheckupDepartment": userChosenDepartment, "userContactNumber": userContactNumber, "userAppointmentCode": randomCode, "DateTime": f"{str(datetime.now())}, {datetime.now().strftime('%H:%M:%S')}"} }
+                queue.append(newPatient)
+                existingFile.seek(0)
+                existingFile.write(json.dumps({"patients": list(queue)}, indent=4))
+                existingFile.close()
+
+                # for printing purposes 
+                patient['patientAppointmentCode'] = newPatient[randomCode]['userAppointmentCode']
+                patient['patientDateTime'] = newPatient[randomCode]['DateTime']
             else:
-                existingFile = open("patients.json", "+")
-                queue = deque(json.load(existingFile))
-                HeadDate = queue[0]['DateTime']
-                queue.append({"patientName": userFullName, "patientCheckupDepartment": userChosenDepartment, "userContactNumber": userContactNumber, "userAppointmentCode": randomCode})
-                pass
-            break
-        elif userInput.lower() in (response.lower() for response in data['Suggestions']['SetAppointment']):
+                queue = deque([])
+                queue.append({randomCode: {"patientName": userFullName, "patientCheckupDepartment": userChosenDepartment, "userContactNumber": userContactNumber, "userAppointmentCode":
+                 randomCode, "DateTime": f"{str(datetime.now())}, {datetime.now().strftime('%H:%M:%S')}"}})
+                newPatientsData = open("patients.json", "w")
+                newPatientsData.write(json.dumps({"patients": list(queue)}, indent=4))
+                newPatientsData.close()
+
+                # for printing purposes 
+                patient['patientAppointmentCode'] = queue[0][randomCode]['userAppointmentCode']
+                patient['patientDateTime'] = queue[0][randomCode]['DateTime']
+                
+            
+            print(f"Your appointment has been processed. Your appointment code # is {patient['patientAppointmentCode']}")
+            print(f"Your scheduled appointment date is {patient['patientDateTime']}")
+            # appointment number here
+            print("Thank you for using our service. Have a great day!\n")
+        elif userInput.lower() in (response.lower() for response in data['Choices']['SetAppointment']):
             print("check existing appointment")
+            break
+        elif userInput.lower() in (response.lower() for response in data['Choices']['ExitProgram']):
+            print("Exiting program. Thank you for using our service!")
             break
         else:
             print("\nSorry, I do not recognize that command!")
-
-    data.close()
+    file.close()
+    
         
         
 
